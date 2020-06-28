8883de2e3d9b4fdeaa9fa418927bbccb79c6c0e5
Carlos Venegas <cvj_2311@hotmail.com>
2020-06-27 22:13:58 -0400

    change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  resApiUrl: 'https://3.80.72.162', // has to be https, uses port 443 by default
+  resApiUrl: 'https://54.235.31.63', // has to be https, uses port 443 by default
   localhost: 'https://symbiocreation-ui.vercel.app'
 };
