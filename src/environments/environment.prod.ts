e71140db96e22ba03208eb34f051d37e6e133717
Carlos Venegas <cvj_2311@hotmail.com>
2020-07-23 19:37:02 -0400

    changed domain name to app.simbiocreacion.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   resApiUrl: 'https://res.symbiocreation.net', // has to be https (port 443 by default)
-  localhost: 'https://symbiocreation-ui.vercel.app',
+  localhost: 'https://app.simbiocreacion.com',
   socketUrl: 'wss://res.symbiocreation.net/rsocket'
 };
