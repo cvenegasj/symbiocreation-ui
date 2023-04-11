7d3c619da2b7f9ff6c17243aa96b3719abb24c13
Carlos Venegas <cvj_2311@hotmail.com>
2023-04-11 16:08:28 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  resApiUrl: 'https://res.symbiocreation.net', // has to be https (port 443 by default)
+  resApiUrl: 'https://res.simbiocreacion-backend.site', // has to be https (port 443 by default)
   localhost: 'https://app.simbiocreacion.com',
   //localhost: 'https://symbiocreation-ui.vercel.app',
-  socketUrl: 'wss://res.symbiocreation.net/rsocket'
+  socketUrl: 'wss://res.simbiocreacion-backend.site/rsocket'
 };
