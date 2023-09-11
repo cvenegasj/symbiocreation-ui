0e4a2702c2a0fad5a454955b67146cbda05edeb8
Carlos Venegas <carlosvenegas@yape.com.pe>
2023-09-10 20:58:43 -0500

    changed property resApiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  resApiUrl: 'https://res.simbiocreacion-backend.site', // has to be https (port 443 by default)
+  resApiUrl: 'https://srv.simbiocreacion.com', // has to be https (port 443 by default)
   localhost: 'https://app.simbiocreacion.com',
   //localhost: 'https://symbiocreation-ui.vercel.app',
-  socketUrl: 'wss://res.simbiocreacion-backend.site/rsocket'
+  socketUrl: 'wss://srv.simbiocreacion.com/rsocket'
 };
